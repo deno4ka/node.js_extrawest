c6b92359e369c50e8f6f5944163ed82fc8719b82
Denys Dvornyi <ddvornyi@geeksforless.net>
2019-03-25 17:19:48 +0200

    added delete task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,27 @@
-function defaultTask(cb) {
+function defaultTask(test) {
     // place code for your default task here
     console.log('defaultTask');
-    cb();
 
+    const gulp = require('gulp');
+
+    // delete required files & folders
+    const del = require('del');
+    (async () => {
+        const deletedPaths = await del(['target/**']);
+
+        console.log('Deleted files and folders:\n', deletedPaths.join('\n'));
+    })();
+
+    // const tslint = require('gulp-tslint');
+    // gulp.task("tslint", () =>
+    //     gulp.src("source.ts")
+    //         .pipe(tslint({
+    //             formatter: "verbose"
+    //         }))
+    //         .pipe(tslint.report())
+    // );
+
+    test();
 }
 
-exports.default = defaultTask
+exports.default = defaultTask;
