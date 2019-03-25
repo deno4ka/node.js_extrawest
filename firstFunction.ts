b82769a5a74178c449518f7c7ea72aea31e89faa
deno4ka <deno4ka@gmail.com>
2019-03-25 11:59:09 +0200

    First commit in my node.js_extrawest repository

---
+++ firstFunction.ts
@@ -0,0 +1,5 @@
+function add(a: number, b: number): number {
+    let result = a + b;
+    console.log('result: ', result);
+    return result;
+}
---
+++ gulpfile.js
@@ -0,0 +1,8 @@
+function defaultTask(cb) {
+    // place code for your default task here
+    console.log('defaultTask');
+    cb();
+
+}
+
+exports.default = defaultTask
