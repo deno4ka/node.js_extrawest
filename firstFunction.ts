70a29c6cccdaaba933d15a215cfc9b30b4525f6f
deno4ka <deno4ka@gmail.com>
2019-03-26 07:41:45 +0200

    fixed tslint, tsconfig.json & gulpfile.js

--- firstFunction.ts
+++
@@ -1,5 +0,0 @@
-function add(a: number, b: number): number {
-    let result = a + b;
-    console.log('result: ', result);
-    return result;
-}
--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,33 @@
-function defaultTask(test) {
-    // place code for your default task here
-    console.log('defaultTask');
+const gulp = require('gulp');
 
-    const gulp = require('gulp');
-
-    // delete required files & folders
-    const del = require('del');
-    (async () => {
-        const deletedPaths = await del(['target/**']);
+const del = require('del');
+gulp.task('del', function() {
+    return (async () => {
+        const deletedPaths = await del(['built/**']);
 
         console.log('Deleted files and folders:\n', deletedPaths.join('\n'));
     })();
+});
 
-    // const tslint = require('gulp-tslint');
-    // gulp.task("tslint", () =>
-    //     gulp.src("source.ts")
-    //         .pipe(tslint({
-    //             formatter: "verbose"
-    //         }))
-    //         .pipe(tslint.report())
-    // );
+const tslint = require('gulp-tslint');
+gulp.task('tslint', () =>
+    gulp.src('src/**')
+        .pipe(tslint({
+            formatter: 'verbose'
+        }))
+        .pipe(tslint.report())
+);
 
-    test();
-}
+const ts = require('gulp-typescript');
+gulp.task('compile', function () {
+    return gulp.src('src/**/*.ts')
+        .pipe(ts({
+            noImplicitAny: true,
+            outFile: 'output.js'
+        }))
+        .pipe(gulp.dest('built/local'));
+});
 
-exports.default = defaultTask;
+gulp.task('all', gulp.series(['del', 'tslint', 'compile']));
+
+gulp.task('default', gulp.series(['all']));
